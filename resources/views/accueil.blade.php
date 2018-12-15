194908887d81f90e95c23db56f3c0951d9ffd2bc
PotatoAim44 <rossie.juliet@gmail.com>
2018-12-15 17:27:03 +0100

    Ajout des photos pour l'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -5,5 +5,18 @@
 @endsection
 
 @section ('contenu')
-	<p>Welcome sur le site</p>
+	<p>Bienvenue sur notre site</p>
+	<table>
+		<tr>
+			<td><img class="img-fluid" alt="Responsive image" src="/images/port1.jpg" /></td>
+			<td>Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit.</td>
+		</tr>
+		<tr>
+			<td>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.</td>
+			<td><img class="img-fluid" alt="Responsive image" src="/images/port2.jpg" /></td>
+		</tr>
+		<tr>
+			<td><img class="img-fluid" alt="Responsive image" src="/images/port3.jpg" /></td>
+			<td>Illud autem non dubitatur quod cum esset aliquando virtutum omnium domicilium Roma, ingenuos advenas plerique nobilium, ut Homerici bacarum suavitate Lotophagi, humanitatis multiformibus officiis retentabant.</td>
+		</tr>
 @endsection
