df567051095b1dd6bf8597af12ab3edf41f50611
PotatoAim44 <rossie.juliet@gmail.com>
2018-12-16 09:15:21 +0100

    Ajout d'un footer MAIS je dois modifier ^lus tard

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="page-footer font-small indigo fixed-bottom">
+	<div class="footer-copyright text-center py-3">© 2018 Copyright:
+		<a href="#"> nom_du_port.com</a>
+	</div>
+</footer>
+
