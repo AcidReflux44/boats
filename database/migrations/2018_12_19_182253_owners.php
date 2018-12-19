35d2122a216eadeb7b63b089e53b1cc4eba20c51
BuildTools <unconfigured@null.spigotmc.org>
2018-12-19 19:27:52 +0100

    Création de la migration des propriétaires des Bâteaux

---
+++ database/migrations/2018_12_19_182253_owners.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Owners extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function(Blueprint $table) {
+
+            $table->integer('user_id');
+            $table->foreign('user_id')->reference('id')->on('users');
+
+            $table->integer('boat_id');
+            $table->foreign('boat_id')->reference('id')->on('boats');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
