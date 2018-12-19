bfed5a3a0a2f52f251222406690360dac3a54be1
BuildTools <unconfigured@null.spigotmc.org>
2018-12-19 19:21:47 +0100

    Création de la migration des réservations

---
+++ database/migrations/2018_12_19_181352_reservations.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Reservations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function(Blueprint $table) {
+
+            $table->integer('place_id');
+            $table->foreign('place_id')->reference('id')->on('places');
+            $table->integer('user_id');
+            $table->foreign('user_id')->reference('id')->on('users');
+            $table->date('date_debut');
+            $table->date('date_fin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
