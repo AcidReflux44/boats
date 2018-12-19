1947114985cd217443761bb496f62553b727fb08
PotatoAim44 <rossie.juliet@gmail.com>
2018-12-19 17:18:50 +0100

    Annulation du dernier commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,58 +1,51 @@
-<?php
-use Auth;
-?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+		<title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+		<!-- Scripts -->
+		<script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+		<!-- Fonts -->
+		<link rel="dns-prefetch" href="//fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<style>
-		/*pour le bouton toggle*/
-		.navbar-header{
-		  width:100%;
+		<!-- Styles -->
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<style>
+			/*pour le bouton toggle*/
+			.navbar-header{
+			  width:100%;
+			}
+			#right-side-button{
+			  float:right;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+		function current_page($uri = "/") {
+			return strstr(request()->path(), $uri);
 		}
-		#right-side-button{
-		  float:right;
-		}
-	</style>
-</head>
-<body>
-	<?php
-	function current_page($uri = "/") {
-		return strstr(request()->path(), $uri);
-	}
-	?>
-    <div class="container-fluid" id="app">
-        <div class="titre-site">
-			<h1>Nom du port</h1>
+		?>
+		<div class="container-fluid" id="app">
+			<div class="titre-site">
+				<h1>Nom du port</h1>
+			</div>
+			
+			<div name="menu-site">
+				@include('includes.header1')
+			</div>
+
+			<main class="py-4">
+				@yield('content')
+			</main>
 		</div>
-		
-		<div name="menu-site">
-		@include('includes.header1')
-		<!--@if(Auth::check())
-			@include('includes.header2')
-		@else
-			@include('includes.header1')
-		</div>-->
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+	</body>
 </html>
