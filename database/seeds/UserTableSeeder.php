0ba4eebf82df288d88a4f91b4d1cb2df69638f95
BuildTools <unconfigured@null.spigotmc.org>
2018-12-20 21:48:44 +0100

    Ajout d'un utilisateur seeder: aaa@aaa.com, aaa (mot de passe)

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+        User::create([
+            'name' => 'AAA',
+            'email' => "aaa@aaa.com",
+            'password' => bcrypt('aaa')
+        ]);
+    }
+}
