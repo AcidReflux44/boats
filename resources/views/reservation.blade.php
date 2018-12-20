2fa5cf597c0910572f5da026d15748bd76e285f8
BuildTools <unconfigured@null.spigotmc.org>
2018-12-20 23:09:20 +0100

    Formulaire réservation (sans design)

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -1,14 +1,88 @@
 @extends('template')
 @section('titre', 'Demande de réservation')
 
-@section('annuel')
-    Taille:
+@section('annuel')	<div class ="row">
+    <div class ="col">
+        {!! Form::open(['route' => 'boats.create'], ['class' => 'form']) !!}
+        <div class ="form-group">
+            {!! Form::label('longueur','Longueur :',
+                ['class'=>'control-label'])
+            !!}
+            {!! Form::text('longueur', null, [
+                    'class'=>'form-control input-lg',
+                    'placeholder'=>'Longueur'
+                ])
+            !!}
+        </div>
+        <div class ="form-group">
+            {!! Form::label('largeur','Largeur :',
+                ['class' => 'control-label'])
+            !!}
+            {!! Form::text('largeur', null, [
+                    'class' => 'form-control input-lg',
+                    'placeholder' => 'Largeur'
+                ])
+            !!}
+        </div>
+        <div class ="form-group">
+            {!! Form::label('date_debut','À partir du :',
+                ['class' => 'control-label'])
+            !!}
+            {!! Form::date('date_debut', date("Y-m-d"), [
+                    'class' => 'form-control input-lg'
+                ])
+            !!}
+        </div>
+        <input type="hidden" name="date_fin" value="{!! date("Y-m-d", strtotime("+1 year")) !!}">
+        {!! Form::submit(); Form::close() !!}
+        </div>
+    </div>
 @endsection
 
 @section('periode')
-    Début:
-    Fin:
-    Taille:
+    <div class ="col">
+        {!! Form::open(['route' => 'boats.create'], ['class' => 'form']) !!}
+        <div class ="form-group">
+            {!! Form::label('longueur','Longueur :',
+                ['class'=>'control-label'])
+            !!}
+            {!! Form::text('longueur', null, [
+                    'class'=>'form-control input-lg',
+                    'placeholder'=>'Longueur'
+                ])
+            !!}
+        </div>
+        <div class ="form-group">
+            {!! Form::label('largeur','Largeur :',
+                ['class' => 'control-label'])
+            !!}
+            {!! Form::text('largeur', null, [
+                    'class' => 'form-control input-lg',
+                    'placeholder' => 'Largeur'
+                ])
+            !!}
+        </div>
+        <div class ="form-group">
+            {!! Form::label('date_debut','À partir du :',
+                ['class' => 'control-label'])
+            !!}
+            {!! Form::date('date_debut', date("Y-m-d"), [
+                    'class' => 'form-control input-lg'
+                ])
+            !!}
+        </div>
+        <div class ="form-group">
+            {!! Form::label('date_fin','Date fin :',
+                ['class' => 'control-label'])
+            !!}
+            {!! Form::date('date_fin', date("Y-m-d", strtotime("+1 month")), [
+                    'class' => 'form-control input-lg'
+                ])
+            !!}
+        </div>
+        {!! Form::submit(); Form::close() !!}
+        </div>
+    </div>
 @endsection
 
 @section('selection')
