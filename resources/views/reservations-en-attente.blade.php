95dcd0aefc606eaf5a3c9bcc36674fef181cb00d
BuildTools <unconfigured@null.spigotmc.org>
2018-12-21 15:52:10 +0100

    Vue de la liste des réservations de l'utilisateur

---
+++ resources/views/reservations-en-attente.blade.php
@@ -0,0 +1,32 @@
+@extends('template')
+@section('titre', 'Réservations en attente')
+
+@section('contenu')
+    <div class="container">
+        <h5 class="my-3">Réservations en attente</h5>
+        <a href="/profil" class="btn btn-info float-right mb-3">Retour au profil</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Port</th>
+                    <th>Longueur</th>
+                    <th>Largeur</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $reservations = App\Boat::all();
+                ?>
+                    @foreach($reservations as $reservation)
+                    <tr scope="row">
+                        <td>{{ $reservation->id }}</td>
+                        <td>Miama Bitch</td>
+                        <td>{{ $reservation->longueur }}</td>
+                        <td>{{ $reservation->largeur }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
